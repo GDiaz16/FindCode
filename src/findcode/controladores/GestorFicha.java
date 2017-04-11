f3682d88d0907e63608c5570125967890ddbe53b
GDiaz16 <egonzalodm@gmail.com>
2017-04-10 19:13:47 -0500

    Correccion error

--- src/findcode/controladores/GestorFicha.java
+++ src/findcode/controladores/GestorFicha.java
@@ -39,7 +39,7 @@ public class GestorFicha {
         this.listaPopUp = listaPopUp;
         cargarPalabras();
         listEvent();
-        setListaIngredientes();
+        //setListaIngredientes();
         
     }
 
